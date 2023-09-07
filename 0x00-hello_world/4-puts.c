97c727c96751c3e856d328aed742fada7bc3616f
Matiyasmulu05 <matiyasmulu69@gmail.com>
2023-09-07 13:09:32 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-//*
- * main - Entry point
+#include <stdio.h>
+
+/**
+ * main - this is the main function
  *
- * Reyurn: always 0 (success)
+ * Return: always 0
  */
- int main(void)
+
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0)
+	puts("\"Programming is like building a multilingual puzzle):
+		Return (0):
 }
