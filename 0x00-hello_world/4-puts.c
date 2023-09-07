10e582bb06ee8d8d20ce3f5e9bc9fb777f82b86d
Matiyasmulu05 <matiyasmulu69@gmail.com>
2023-09-07 13:35:32 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * Return: always 0
+ * REturn: always 0
  */
 
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle):
-		Return (0):
+{ 
+	puts("\"Programming is like building a multilingual puzzle);
+		return (0);
 }
