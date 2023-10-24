a145449726d28bf9ea9114888eb984b263ee5ef4
Matiyasmulu05 <matiyasmulu69@gmail.com>
2023-10-24 08:12:02 -0700

    M

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the first node of a list
+ * its data
+ * @head: the pointer
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *tmp;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	n = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+	return (n);
+}
