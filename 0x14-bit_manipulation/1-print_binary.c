72a35d8a9f252a6a88a7da2c6ef47824fd81a592
Matiyasmulu05 <matiyasmulu69@gmail.com>
2023-10-26 10:19:48 -0700

    M

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _pow - func calculates (base power)
+ * @base: base
+ * @power: power
+ *
+ * Return: value of base and power
+ */
+
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int M;
+
+	num = 1;
+	for (M = 1; M <= power; M++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - prints a number
+ * @n: number to print
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
